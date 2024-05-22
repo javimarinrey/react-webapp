96807f356ed28fb56b2ba0165df26ce71ac2d703
jmr <javier.marin@igmweb.com>
2024-05-22 18:07:54 +0200

    commit

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import Menu from "../components/Menu";
+import MenuVertical from "../components/MenuVertical";
 
 export default function Main() {
+
+    const widthMax = 991;
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+          setWidth(window.innerWidth);
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
     return (
         <div>    
-            <Menu/>
+            {widthMax >= width && <MenuVertical/> }
+            {widthMax < width && <Menu/> }
             <br />
             <div className="container">
                 <Outlet/>
