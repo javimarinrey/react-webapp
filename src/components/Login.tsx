717dc704045f7e05c43dac380382498654e3757e
jmr <javier.marin@igmweb.com>
2024-05-22 18:58:14 +0200

    commit

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,28 +1,38 @@
-import {Link} from "react-router-dom";
-import React, {MouseEventHandler, useState} from "react";
-import {UserContext} from "../context/UserContext";
-import {IUser, UserContextType} from "../interfaces/IUser";
+import { Link } from "react-router-dom";
+import React, { MouseEventHandler, useState } from "react";
+import { UserContext } from "../context/UserContext";
+import { IUser, UserContextType } from "../interfaces/IUser";
 
 export default function Login() {
 
-    const {user, saveUser} = React.useContext(UserContext) as UserContextType;
+    const { user, saveUser } = React.useContext(UserContext) as UserContextType;
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-    const handlerLogin = (event:React.MouseEvent<HTMLButtonElement>) => {
+    const handlerLogin = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
         if (username.length > 0 && password.length > 0) {
             localStorage.setItem('token', '11111');
-            const user: IUser = {username: username, isAuth: true};
+            const user: IUser = { username: username, isAuth: true };
             saveUser(user)
         }
     }
 
-    return(
-        <div className="row">
-            <div className="col-auto"><input type="text" className="form-control" placeholder="Username" onChange={(e)=> {setUsername(e.target.value)}}/></div>
-            <div className="col-auto"><input type="password" className="form-control" placeholder="Password" onChange={(e)=> {setPassword(e.target.value)}}/></div>
-            <div className="col-auto"><button type="button" className="btn btn-primary" onClick={handlerLogin}>Entrar</button></div>
-            <div className="col-auto"><Link to={'/register'} className="btn btn-link">¿No tienes cuenta?</Link></div>
+    return (
+        <div className="card">
+            <div className="card-header">Iniciar sesión</div>
+            <div className="card-body">
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Nombre de usuario</label>
+                    <input type="text" className="form-control" placeholder="" onChange={(e) => { setUsername(e.target.value) }} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Contraseña</label>
+                    <input type="password" className="form-control" placeholder="" onChange={(e) => { setPassword(e.target.value) }} />
+                </div>
+
+                <button type="button" className="btn btn-primary" onClick={handlerLogin}>Entrar</button>
+                <Link to={'/register'} className="btn btn-link">¿No tienes cuenta?</Link>
+            </div>
         </div>
     )
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import {UserContext} from "../context/UserContext";
-import {UserContextType} from "../interfaces/IUser";
+import { UserContext } from "../context/UserContext";
+import { UserContextType } from "../interfaces/IUser";
 import Login from "../components/Login";
 
 export default function HomeView() {
-    const {user, saveUser} = React.useContext(UserContext) as UserContextType;
+    const { user, saveUser } = React.useContext(UserContext) as UserContextType;
     console.log(`Home - User ${JSON.stringify(user)}`);
     return (
         <div>
-            {user.isAuth && <div>User OK</div>}
-            {!user.isAuth && <Login/>}
+            <div className="row">
+                <div className="col">Home</div>
+                <div className="col">
+                    {user.isAuth && <div>User OK</div>}
+                    {!user.isAuth && <Login />}
+                </div>
+            </div>
         </div>
     )
 }
